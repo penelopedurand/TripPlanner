415723bf882359d6bed70225652b4c881dd9df4b
Penelope Durand <penelope.durand@mail.goucher.edu>
2022-05-12 01:06:29 -0400

    css changes

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./styles.css";
 
 function Home() {
     return (
-        <div>
-            <h1>Welcome to the Homepage!</h1>
+        <div className="home-div">
+            <h1 className="welcome">Welcome to the Homepage!</h1>
         </div>
     )
 }
