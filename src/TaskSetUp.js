de31c513690100d45e18382e2d8623669e7facbb
Patrick Hart <pathart612@gmail.com>
2022-05-12 10:08:34 -0400

    code-clean-up

--- src/TaskSetUp.js
+++ src/TaskSetUp.js
@@ -1,28 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Alert } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import TaskCard from "./TaskCard";
 import TaskView from "./TaskView";
 import TaskFormAssign from "./TaskFormAssign"
 
 function TaskSetUp({ trips, addNewTask , newTrip, sendTaskAsToView}) {
     
-
-
-
-    let tripObj = trips.map(trip => {
-        return (
-            <TaskCard
-                key={trip.id}
-                name={trip.name}
-                food={trip.food}
-                transportation={trip.transportation}
-                lodging={trip.lodging}
-                activities={trip.activities}
-                misc={trip.misc}
-            />
-        )
-    })
 
     return (
         <>
@@ -31,7 +14,6 @@ function TaskSetUp({ trips, addNewTask , newTrip, sendTaskAsToView}) {
             </div>
             <div>
                 <TaskFormAssign addNewTask={addNewTask} trips={trips} sendTaskAsToView={sendTaskAsToView}/>
-                {/* {tripObj} */}
             </div>
             <TaskView newTrip={newTrip}></TaskView>
         </>
